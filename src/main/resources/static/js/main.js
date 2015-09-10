481c16372711fdec258e06e91cf396e314e9efa0
bsmk <miroslavmatejovsky@gmail.com>
2015-09-10 06:50:56 +0200

    added retrieving data from server

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,16 @@
-function onLoad() {
-  var svg = d3.select("svg");
-  var circle = svg.selectAll("circle").data([32, 57, 112, 293, 13, 864]);
-  var circleEnter = circle.enter().append("circle")
+function refresh() {
+  $.ajax({
+    url: "/api/data",
+    success: function(data) {
+      var svg = d3.select("svg");
+      var circle = svg.selectAll("circle").data(jQuery.parseJSON(data));
+      var circleEnter = circle.enter().append("circle")
 
-  circle.attr("r", function(d) { return Math.sqrt(d); });
+      circle.attr("r", function(d) { return Math.sqrt(d); });
 
-  circleEnter.attr("cy", 60);
-  circleEnter.attr("cx", function(d, i) { return i * 100 + 30; });
-  circleEnter.attr("r", function(d) { return Math.sqrt(d); });
-
+      circleEnter.attr("cy", 60);
+      circleEnter.attr("cx", function(d, i) { return 40*i + 40; });
+      circleEnter.attr("r", function(d) { return Math.sqrt(d); });
+    }
+  });
 }
