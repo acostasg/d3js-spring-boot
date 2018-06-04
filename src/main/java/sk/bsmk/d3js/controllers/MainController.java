b3679f4e062e74383e5ef70e66c737dc83c5b051
Albert <acostasg@uoc.edu>
2018-06-04 23:59:45 +0200

    Add sample to json dataset to print plot

--- src/main/java/sk/bsmk/d3js/controllers/MainController.java
+++ src/main/java/sk/bsmk/d3js/controllers/MainController.java
@@ -1,27 +1,108 @@
 package sk.bsmk.d3js.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ThreadLocalRandom;
-
-/**
- * Created by bsmk on 9/9/15.
- */
 @RestController
 public class MainController {
 
-  @RequestMapping("/api/data")
-  public String data() {
-    final ThreadLocalRandom random = ThreadLocalRandom.current();
-    return String.format(
-        "[%d, %d, %d, %d, %d]",
-        random.nextInt(200),
-        random.nextInt(200),
-        random.nextInt(200),
-        random.nextInt(200),
-        random.nextInt(200)
-    );
-  }
+    @RequestMapping(value = "/api/data", method = RequestMethod.GET, produces = "application/json")
+    public String data() {
+        return "[{" +
+                "    \"nombre\": \"Alemania\"," +
+                "    \"salarioMedio\": \"45952\"" +
+                "}, {" +
+                "    \"nombre\": \"Austria\"," +
+                "    \"salarioMedio\": \"42573\"" +
+                "}, {" +
+                "    \"nombre\": \"Bulgaria\"," +
+                "    \"salarioMedio\": \"5080\"" +
+                "}, {" +
+                "    \"nombre\": \"BÃ©lgica\"," +
+                "    \"salarioMedio\": \"46464\"" +
+                "}, {" +
+                "    \"nombre\": \"Chipre\"," +
+                "    \"salarioMedio\": \"22337\"" +
+                "}, {" +
+                "    \"nombre\": \"Croacia\"," +
+                "    \"salarioMedio\": \"12232\"" +
+                "}, {" +
+                "    \"nombre\": \"Dinamarca\"," +
+                "    \"salarioMedio\": \"53319\"" +
+                "}, {" +
+                "    \"nombre\": \"Eslovaquia\"," +
+                "    \"salarioMedio\": \"10342\"" +
+                "}, {" +
+                "    \"nombre\": \"Eslovenia\"," +
+                "    \"salarioMedio\": \"17851\"" +
+                "}, {" +
+                "    \"nombre\": \"EspaÃ±a\"," +
+                "    \"salarioMedio\": \"26162\"" +
+                "}, {" +
+                "    \"nombre\": \"Estonia\"," +
+                "    \"salarioMedio\": \"12436\"" +
+                "}, {" +
+                "    \"nombre\": \"Finlandia\"," +
+                "    \"salarioMedio\": \"42910\"" +
+                "}, {" +
+                "    \"nombre\": \"Francia\"," +
+                "    \"salarioMedio\": \"37427\"" +
+                "}, {" +
+                "    \"nombre\": \"Grecia\"," +
+                "    \"salarioMedio\": \"20168\"" +
+                "}, {" +
+                "    \"nombre\": \"Holanda\"," +
+                "    \"salarioMedio\": \"48856\"" +
+                "}, {" +
+                "    \"nombre\": \"HungrÃ\u00ADa\"," +
+                "    \"salarioMedio\": \"9748\"" +
+                "}, {" +
+                "    \"nombre\": \"Irlanda\"," +
+                "    \"salarioMedio\": \"34466\"" +
+                "}, {" +
+                "    \"nombre\": \"Islandia\"," +
+                "    \"salarioMedio\": \"44210\"" +
+                "}, {" +
+                "    \"nombre\": \"Italia\"," +
+                "    \"salarioMedio\": \"30463\"" +
+                "}, {" +
+                "    \"nombre\": \"Letonia\"," +
+                "    \"salarioMedio\": \"9830\"" +
+                "}, {" +
+                "    \"nombre\": \"Lituania\"," +
+                "    \"salarioMedio\": \"8116\"" +
+                "}, {" +
+                "    \"nombre\": \"Luxemburgo\"," +
+                "    \"salarioMedio\": \"54560\"" +
+                "}, {" +
+                "    \"nombre\": \"Malta\"," +
+                "    \"salarioMedio\": \"20500\"" +
+                "}, {" +
+                "    \"nombre\": \"Noruega\"," +
+                "    \"salarioMedio\": \"64922\"" +
+                "}, {" +
+                "    \"nombre\": \"Polonia\"," +
+                "    \"salarioMedio\": \"10124\"" +
+                "}, {" +
+                "    \"nombre\": \"Portugal\"," +
+                "    \"salarioMedio\": \"17436\"" +
+                "}, {" +
+                "    \"nombre\": \"Reino Unido\"," +
+                "    \"salarioMedio\": \"44203\"" +
+                "}, {" +
+                "    \"nombre\": \"RepÃºblica Checa\"," +
+                "    \"salarioMedio\": \"11334\"" +
+                "}, {" +
+                "    \"nombre\": \"RumanÃ\u00ADa\"," +
+                "    \"salarioMedio\": \"6222\"" +
+                "}, {" +
+                "    \"nombre\": \"Suecia\"," +
+                "    \"salarioMedio\": \"44840\"" +
+                "}, {" +
+                "    \"nombre\": \"Suiza\"," +
+                "    \"salarioMedio\": \"74528\"" +
+                "}]";
+    }
 
 }
